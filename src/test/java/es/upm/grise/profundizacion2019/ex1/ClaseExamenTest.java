3bf1a5b4656e0a82a1d557fb03043ddea39980e4
RafaIG <rafael.ines.guillen@gmail.com>
2019-10-29 16:08:21 +0100

    Examen Finalizado - Rafael Ines Guillen

--- src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
+++ src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
@@ -4,11 +4,43 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import es.upm.grise.profundizacion2019.ex1.ClaseExamen;;
+
 public class ClaseExamenTest {
 
+	private ClaseExamen exam = new ClaseExamen();
+
+	/**
+	 * Primera condicion falsa
+	 * 
+	 * Segunda condicion verdadera
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1() {
+		assertEquals(0, exam.metodoAProbar(0, 1), 0);
 	}
+
+	/**
+	 * Primera condicion verdadera
+	 * 
+	 * Segunda condicion verdadera
+	 */
+	@Test
+	public void test2() {
+		assertEquals(0, exam.metodoAProbar(2, 1), 0);
+	}
+
+	/**
+	 * Primera condicion no importa
+	 * 
+	 * Segunda condicion falsa. Es inalcanzable.
+	 * 
+	 * Como no se puede realizar esta prueba; el codigo nunca tendra el 100% de
+	 * cobertura
+	 */
+//	@Test
+//	public void test3() {
+//		assertEquals(1, exam.metodoAProbar(2, 1), 0);
+//	}
 
 }
